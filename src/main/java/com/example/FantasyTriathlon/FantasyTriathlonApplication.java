e535ce8403de4b1a72888e4dae6b2b969e1c0528
Aine Kearns <aine.kearns@hotmail.ca>
2020-09-24 16:56:05 +0200

    skeleton for project

---
+++ src/main/java/com/example/FantasyTriathlon/FantasyTriathlonApplication.java
@@ -0,0 +1,13 @@
+package com.example.FantasyTriathlon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyTriathlonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyTriathlonApplication.class, args);
+	}
+
+}
